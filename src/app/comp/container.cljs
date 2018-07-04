0cca8473ce3cf2652a6788379e12980391d49818
ChenYong <chenyong@jimeng.io>
2018-07-04 15:02:41 +0800

    handle main UI

--- src/app/comp/container.cljs
+++ src/app/comp/container.cljs
@@ -22,7 +22,29 @@
             [respo-md.comp.md :refer [comp-md]]
             [app.config :refer [dev?]]
             ["icons" :as icons]
-            [clojure.string :as string]))
+            ["copy-text-to-clipboard" :as copy]
+            ["shortid" :as shortid]
+            [clojure.string :as string]
+            [respo-message.comp.messages :refer [comp-messages]]
+            [respo-message.action :as action]))
+
+(defcomp
+ comp-header
+ (content)
+ (div
+  {:style {:padding 16, :border-bottom (str "1px solid " (hsl 0 0 96)), :text-align :center}}
+  (<> "FontAwesome 4.7 icons" {:font-family ui/font-fancy, :font-size 16, :font-weight 300})
+  (=< 16 nil)
+  (input
+   {:style (merge ui/input {:width 400}),
+    :value content,
+    :placeholder "Filter...",
+    :on-input (fn [e d! m!] (d! :content (:value e)))})))
+
+(defn copy-text! [text d!]
+  (let [token (.generate shortid)]
+    (d! action/create {:text (str "Copied: " text), :token token})
+    (js/setTimeout #(d! action/remove-one {:token token}) 2000)))
 
 (defcomp
  comp-icon
@@ -30,36 +52,47 @@
  (div
   {:style (merge
            ui/center
-           {:width 120, :height 120, :border (str "1px solid " (hsl 0 0 96))})}
-  (div
-   {:style {:text-align :center, :font-size 24, :padding 8}}
-   (i {:style {}, :class-name (str "fa fa-" code)}))
-  (div
-   {:style (merge ui/column {:text-align :center, :font-size 12})}
-   (<> icon-name)
-   (<> code))))
+           {:width 120,
+            :height 120,
+            :border (str "1px solid " (hsl 0 0 98)),
+            :font-size 12,
+            :font-family ui/font-normal,
+            :color (hsl 0 0 70)})}
+  (i
+   {:style {:text-align :center, :font-size 24, :padding 8, :color :black},
+    :class-name (str "fa fa-" code)})
+  (span
+   {:inner-text icon-name,
+    :style {:cursor :pointer},
+    :on-click (fn [e d! m!] (copy-text! icon-name d!))})
+  (span
+   {:inner-text code,
+    :style {:cursor :pointer, :font-size 10},
+    :on-click (fn [e d! m!] (copy-text! code d!))})))
 
 (def icons-dict (js->clj icons))
 
 (defcomp
  comp-container
  (reel)
- (let [store (:store reel), states (:states store), state (or (:data states) {:query ""})]
+ (let [store (:store reel), states (:states store)]
    (div
-    {:style (merge ui/global ui/column)}
-    (div
-     {}
-     (<> "Header")
-     (input
-      {:style ui/input,
-       :value (:query state),
-       :placeholder "Filter...",
-       :on-input (fn [e d! m!] (m! (assoc state :query (:value e))))}))
-    (div
-     {}
-     (list->
-      {:style (merge ui/row {:flex-wrap :wrap})}
-      (->> icons-dict
-           (filter (fn [[icon-name code]] (string/includes? code (:query state))))
-           (map (fn [[icon-name icon-code]] [icon-name (comp-icon icon-name icon-code)])))))
+    {:style (merge ui/global ui/fullscreen ui/column)}
+    (comp-header (:content store))
+    (list->
+     {:style (merge
+              ui/row
+              ui/flex
+              {:flex-wrap :wrap,
+               :padding "32px 16px 80px 16px",
+               :justify-content :center,
+               :overflow :auto,
+               :align-items :flex-start})}
+     (->> icons-dict
+          (filter (fn [[icon-name code]] (string/includes? code (:content store))))
+          (map (fn [[icon-name icon-code]] [icon-name (comp-icon icon-name icon-code)]))))
+    (comp-messages
+     (:messages store)
+     {:bottom? false}
+     (fn [info d! m!] (d! action/remove-one info)))
     (when dev? (cursor-> :reel comp-reel states reel {})))))
--- src/app/updater.cljs
+++ src/app/updater.cljs
@@ -1,9 +1,14 @@
 
-(ns app.updater (:require [respo.cursor :refer [mutate]]))
+(ns app.updater
+  (:require [respo.cursor :refer [mutate]]
+            [respo-message.action :as action]
+            [respo-message.updater :refer [update-messages]]))
 
 (defn updater [store op op-data op-id op-time]
-  (case op
-    :states (update store :states (mutate op-data))
-    :content (assoc store :content op-data)
-    :hydrate-storage op-data
-    store))
+  (if (action/message-action? op)
+    (update store :messages #(update-messages % op op-data op-id op-time))
+    (case op
+      :states (update store :states (mutate op-data))
+      :content (assoc store :content op-data)
+      :hydrate-storage op-data
+      store)))
